ffcb0505c2b6794eba82430798795b76b9ea6661
Webbney Vallon <webbvallon1999@gmail.com>
2025-09-11 23:33:32 -0400

    initial commit

---
+++ Dice game/Program.cs
@@ -0,0 +1,99 @@
+﻿// Identify a dice game, Ceelo (NYC dice game)
+
+// Ceelo is a dice game where people roll a set of 3d6's and if you roll a 4-5-6 it's an auto win, but if you roll 1-2-3 its and auto loss. players can gain single points by getting 2 of a kind in a single roll, exp roll could be 1-1-2 (results in a point)
+
+// Describe dice 
+// top of the dice a number between 1- 6
+// number of sides is 6 (A d6)
+
+// Dice behavior -
+// 3 are rolled
+
+// UML Dice Class
+
+// private members
+// - topSide:int
+// - numOfSides:int
+
+// public members
+// +Dice()
+// +Dice(int)
+// +roll():void
+// +TopSide:int
+// +NumofSide:int  
+
+//Test Case #1
+// 1. Create a Dice object using the default constructor. 
+// 2. Create a Dice object using with a positive number of sides.
+// 3. Create a Dice ojject using with a non-positive number sides.
+//4. Check the TopSide of the dice. Ensure it's within [1 to the maxsides].
+
+
+
+using System.Runtime.CompilerServices;
+
+public class Dice
+{
+    private int topSide;
+    private int numOfSides;
+
+
+    public Dice()
+    {
+        numOfSides = 6;
+    }
+
+    public Dice(int sides)
+    {
+        if (sides < 1)
+        {
+            numOfSides = 6;
+        }
+        else
+        {
+         
+        numOfSides = sides;
+        }
+    }
+
+    public void roll()
+    {
+        Random dice = new Random();
+        topSide = dice.Next(1, numOfSides+1);
+    }
+    
+    public int TopSide {get { return topSide;}}
+}
+
+public class DiceGame()
+{
+    public static void Main()
+    {
+        // test case #1, dice object with a default constructor
+        Dice dice1 = new Dice();
+        dice1.roll();
+        Console.WriteLine("rolling dice for case 1");
+        Console.WriteLine(dice1.TopSide);
+
+
+        //test case #2, dice object with a positive number of sides 
+        Dice dice2 = new Dice(20);
+        dice2.roll();
+        Console.WriteLine("\nrolling dice for case 2");
+        Console.WriteLine(dice2.TopSide);
+
+        //test case #3, dice object with a non-positive number of sides
+         Dice dice3 = new Dice(-1);
+        dice3.roll();
+        Console.WriteLine("\nrolling dice for case 3");
+        Console.WriteLine(dice3.TopSide);
+
+
+
+
+
+        //test case #4,
+    }
+
+
+}
